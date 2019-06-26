65e864b3bb111007cfcf167c4edb0f7934453134
Jhon Wang <wuf31@wuf31.x1c6.jhon.xyz>
2019-06-27 00:44:24 +0700

    fish bash compatibility

--- .config/fish/shared.fish
+++ .config/fish/shared.fish
@@ -8,5 +8,41 @@ set -gx YEAR (date +%Y)
 # Aliases
 alias vim='nvim'
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-alias j='vim ~/data/archive/$YEAR/journal.md'
+alias j='vim ~/archive/$YEAR/journal.md'
 
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+# enables $?
+function bind_status
+  commandline -i (echo '$status')
+end
+
+# enables $$
+function bind_self
+  commandline -i (echo '%self')
+end
+
+# enable keybindings
+function fish_user_key_bindings
+  bind ! bind_bang
+  bind '$' bind_dollar
+  bind '$?' bind_status
+  bind '$$' bind_self
+end
